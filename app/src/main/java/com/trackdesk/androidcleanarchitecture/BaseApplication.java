57feffd597c27c5d90d3f1d8e5b76236a6ea5b97
Otniel <otnieldocs@gmail.com>
2019-09-03 13:10:07 +0700

    add base application, add new packages into data and domain module

---
+++ app/src/main/java/com/trackdesk/androidcleanarchitecture/BaseApplication.java
@@ -0,0 +1,11 @@
+package com.trackdesk.androidcleanarchitecture;
+
+import android.app.Application;
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
